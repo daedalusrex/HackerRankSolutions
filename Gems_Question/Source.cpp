46346f6a41dd8edabb9257d020a2f7680bda53e6
Jared <jaredbc@umich.edu>
2017-12-05 21:33:00 -0500

    Questoins from past

---
+++ Gems_Question/Source.cpp
@@ -0,0 +1,11 @@
+//Asked two questions
+//
+//
+//Find Needle inside of haystack(which was easy but I didn;t look up any of the string stuff)
+//
+//
+//Then did (and I can't believe I finished but I did )
+// A deep copy of a tree. had to use a map, which saved me in the end
+//honestly not sure if I actually finished it correctly, as went 10 mins over, but I did it fin
+
+//Sadly he closed the interview before I could swipe my code back
