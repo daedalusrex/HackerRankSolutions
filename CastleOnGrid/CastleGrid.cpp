91335b93248cb0e66f386b6e9ddedf17d2c7ade5
Jared Combs <jaredcombs5@live.com>
2017-12-21 13:26:59 -0500

    running but incorrect

--- CastleOnGrid/CastleGrid.cpp
+++ CastleOnGrid/CastleGrid.cpp
@@ -9,37 +9,32 @@ using namespace std;
 
 static int gridsize = 0;
 
-bool CellAllowed(vector<string> &grid, vector<vector<int>> &steps,pair<int, int> cell)
+bool CellAllowed(vector<string> &grid, vector<vector<int>> &steps, pair<int, int> cell)
 {
-	string s = grid[cell.first].at(cell.second);
-
-	if (cell.first > 0 && cell.first < gridsize && cell.second > 0 && cell.second < gridsize
-		&& s == "." && steps[cell.first][cell.second] == 0)
-		return true;
-
+	if (cell.first >= 0 && cell.first < gridsize && cell.second >= 0 && cell.second < gridsize)
+	{
+		char test = grid[cell.first].at(cell.second);
+		if (test == '.' && steps[cell.first][cell.second] == 0)
+			return true;
+	}
 	return false;
 }
 
-void EnqueueCell(queue<pair<int, int>> &Q, vector<vector<int>> & allsteps, pair<int, int> cell, int stepcount)
-{
-	Q.push(cell);
-	allsteps[cell.first][cell.second] = stepcount;
-}
-
 int minimumMoves(vector <string> grid, int startX, int startY, int goalX, int goalY) {
-		
+
 	//Clearly a BFS 
 	queue<pair<int, int>> search;
 	search.push(make_pair(startX, startY));
-	
+
 	//create new grid to keep of step counts
 	vector<vector<int>> allsteps(gridsize, vector<int>(gridsize, 0));
 
-	
+
 
 	//plan is to rewrite each cell, with step value, stop and return when you get to goal
 	while (!search.empty() && allsteps[goalX][goalY] == 0)
 	{
+
 		pair<int, int> cell = search.front();
 		search.pop();
 
@@ -52,9 +47,13 @@ int minimumMoves(vector <string> grid, int startX, int startY, int goalX, int go
 		neighbors.push_back(make_pair(cell.first, cell.second - 1));
 		neighbors.push_back(make_pair(cell.first, cell.second + 1));
 
-		for (auto& it : neighbors) {
-			if (CellAllowed(grid, allsteps, it))
-				EnqueueCell(search, allsteps, it, steps);
+		for (vector<pair<int, int>>::iterator it = neighbors.begin(); it != neighbors.end(); it++) {
+			if (CellAllowed(grid, allsteps, *it))
+			{
+				search.push(make_pair(it->first, it->second));
+				allsteps[it->first][it->second] = steps;
+				//cout << "Cell(x,y) " << it->first << " " << it->second << " step" << allsteps[it->first][it->second] << endl;
+			}
 		}
 
 	}
@@ -76,6 +75,7 @@ int main() {
 	int goalX;
 	int goalY;
 	cin >> startX >> startY >> goalX >> goalY;
+
 	int result = minimumMoves(grid, startX, startY, goalX, goalY);
 
 	cout << result << endl;
