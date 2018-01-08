587edb55c691e75fbc3505f9e525581d74a9f4c9
Jared Combs <jaredcombs5@live.com>
2018-01-08 17:05:33 -0500

    First try at Contacts (no success)

--- Contacts_Trie/Contacts.cpp
+++ Contacts_Trie/Contacts.cpp
@@ -28,31 +28,100 @@ using namespace std;
 class t_node
 {
 public:
-	t_node(char c) : data(c),is_word(false), num_prefix(0) {};
-	t_node() :data('\0'), is_word(false), num_prefix(0) {};
+	t_node(char c) : data(c),is_word(false), num_prefix(0), children() {};
+	t_node() :data('\0'), is_word(false), num_prefix(0), children() {};
 
 	char data;
-	//could do a vector of children? , at most 26 letters
-	//could do some cool ascii indexing stuff
-	//root might neeed to be a list of 26 nodes?
 
-	//get's incremented every insert
+	//get's incremented every insert, and traverse
 	int num_prefix;
 
+	//true if a word ends at this node
 	bool is_word;
+
 	vector<t_node*> children;
 	//t_node * childs[26];
 
 
-
+	t_node* ChildHasLetter(char test)
+	{
+		for (auto it : children)
+		{
+			if (it->data == test)
+				return it;
+		}
+		return NULL;
+	}
 };
 
+
+void new_entry(t_node root, string contact)
+{
+	t_node* next = &root;
+	t_node* old = next;
+	int i = 0;
+
+	do {
+		next = old->ChildHasLetter(contact[i]);
+		if (next) 
+		{
+			next->num_prefix++;
+			old = next;
+			i++;
+		}
+		
+	} while (next != NULL && i < contact.size());
+
+	
+	while (i < contact.size())
+	{
+		next = new t_node(contact[i]);
+		old->num_prefix++;
+		old->children.push_back(next);
+
+		if (i == contact.size() - 1)
+			next->is_word = true;
+
+		old = next;
+		i++;
+	}
+}
+
+int find_numContacts(t_node root, string contact)
+{
+	int cur_names = 0;
+	//traverse tree, until out of contact or out of children
+	//out of children print 0
+	//out of contact, print num prefix at current node
+
+	t_node *old = &root;
+	t_node * current = old;
+	int i = 0;
+
+	while (current != NULL && i < contact.size())
+	{
+		current = old->ChildHasLetter(contact[i]);
+		if (current)
+		{
+			cur_names = current->num_prefix;
+		}
+		
+		old = current;
+		i++;
+
+	}
+
+	if (current == NULL)
+		cur_names = 0;
+
+	return cur_names;
+}
 
 
 int main() {
 	int n;
 	//Init trie
-	t_node root();
+	t_node root;
 
 	cin >> n;
 	for (int a0 = 0; a0 < n; a0++) {
@@ -60,11 +129,11 @@ int main() {
 		string contact;
 		cin >> op >> contact;
 
-		if(op == "add")
-			//do addition
+		if (op == "add")
+			new_entry(root, contact);
 
-		if(op == "find")
-			//do find
+		if (op == "find")
+			cout << find_numContacts(root, contact) << endl;
 
 	}
 	return 0;
