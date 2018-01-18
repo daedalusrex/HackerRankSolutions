3c15f4479e0484d8cfc829cd42368d1a89b240d8
Jared Combs <jaredcombs5@live.com>
2018-01-18 15:28:02 -0500

    Roads and Libs SOLVED

--- RoadsAndLibraries/RoadsAndLibraries.cpp
+++ RoadsAndLibraries/RoadsAndLibraries.cpp
@@ -7,7 +7,11 @@
 
 using namespace std;
 
-int roadsAndLibraries(int n, int c_lib, int c_road, vector < vector<int> > cities) {
+long roadsAndLibraries(int n, long c_lib, long c_road, vector < vector<int> > cities) {
+	
+	if (c_lib < c_road)
+		return c_lib * n;
+	
 	vector<vector<int>>  roadmap(n, vector<int>());
 	vector<bool> visited(n, false);
 		
@@ -48,12 +52,12 @@ int roadsAndLibraries(int n, int c_lib, int c_road, vector < vector<int> > citie
 		disj_sets.push_back(new_set);
 	}
 
-	int cost = 0;
+	long cost = 0;
 	//build 1 library in every disj set
 	cost += disj_sets.size() * c_lib;
 	//build just enough roads to connect all cities
 	for (auto it : disj_sets)
-		cost += c_road * it.size() - 1;
+		cost += c_road * (it.size() - 1);
 
 	return cost;
 }
@@ -73,7 +77,7 @@ int main() {
 				cin >> cities[cities_i][cities_j];
 			}
 		}
-		int result = roadsAndLibraries(n, c_lib, c_road, cities);
+		long result = roadsAndLibraries(n, c_lib, c_road, cities);
 		cout << result << endl;
 	}
 	return 0;
