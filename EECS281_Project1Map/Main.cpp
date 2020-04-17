5d4027a635cb248f0a1f53d9ddf3ba38e478991a
Jaco Omen PC <jaredcombs5@live.com>
2020-04-18 01:36:30 +0900

    After much research and checking all the stuff, implemented very efficeint c style array usage basics for map in project. Works and is solid

---
+++ EECS281_Project1Map/Main.cpp
@@ -0,0 +1,176 @@
+#include <map>
+#include <set>
+#include <list>
+#include <cmath>
+#include <ctime>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <string>
+#include <bitset>
+#include <cstdio>
+#include <limits>
+#include <vector>
+#include <climits>
+#include <cstring>
+#include <cstdlib>
+#include <fstream>
+#include <numeric>
+#include <sstream>
+#include <iostream>
+#include <algorithm>
+#include <unordered_map>
+
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <assert.h>
+
+using namespace std;
+
+//#define Levels 3
+//#define Rows 4
+//#define Columns 6
+
+int main(void) {
+
+	size_t Levels = 3;
+	size_t Rows = 4;
+	size_t Columns = 6;
+
+
+    char* map = new char[Levels * Rows * Columns];
+	int counter = 0;
+    	// assign values to allocated memory
+	for (int i = 0; i < Levels; i++)
+	{
+		cout << "Level " << i << endl;
+		for (int j = 0; j < Rows; j++)
+		{
+			for (int k = 0; k < Columns; k++)
+			{
+				map[i*Rows*Columns + j*Columns + k] = 'a' + counter;
+				//*(map + i * Rows * Columns + j * Columns + k) = 'a' + counter;
+				cout << *(map + i * Rows * Columns + j * Columns + k) << " ";
+				counter = (counter+1) % ('z'-'a' + 1);
+			}
+			cout << "\n";
+		}
+	}
+	
+
+
+	//test memset, confirmed it works
+	memset(map, 'X', Levels * Rows * Columns);
+	for (int i = 0; i < Levels; i++)
+	{
+		cout << "Level " << i << endl;
+		for (int j = 0; j < Rows; j++)
+		{
+			for (int k = 0; k < Columns; k++)
+				cout << *(map + i * Rows * Columns + j * Columns + k) << " ";
+			cout << "\n";
+		}
+	}
+
+
+	delete[] map;
+	return 0;
+}
+
+
+
+/*
+Can I do this in C++??  -- Basically no, but the templates trick allows it 
+
+After much reading, I have determined that indeed if doing a flat array
+there is no way to use the brackets instead of pointer math
+because the compiler has no way of knowing the size of the dynamic array provided 
+at run time, and so it cannot translate the brackets into actual math operations on memory
+which is what the programmer must do. Casting will not work for the same reason.
+Templates also occur at compile time.
+Or, of course, just use an STL/boost class which have already been built to solve these problems
+
+C has VLA's and C++ does not 
+So in fact they are sort of making us do this the hard or wrong way
+because they wanted us to learn, and not just get the STL
+
+
+void arr_alloc (size_t x, size_t y, int(**aptr)[x][y])
+{
+  *aptr = malloc( sizeof(int[x][y]) ); // allocate a true 2D array
+  assert(*aptr != NULL);
+}
+
+
+template<size_t size1, size_t size2>
+int sub (int (&a)[size1][size2]) {
+    // ...
+}
+int main() {
+    int a[2][2];
+    sub(a);
+}
+
+void arr_fill (size_t x, size_t y, int array[x][y])
+{
+  for(size_t i=0; i<x; i++)
+  {
+    for(size_t j=0; j<y; j++)
+    {
+      array[i][j] = (int)j + 1;
+    }
+  }
+}
+
+void arr_print (size_t x, size_t y, int array[x][y])
+{
+  for(size_t i=0; i<x; i++)
+  {
+    for(size_t j=0; j<y; j++)
+    {
+      printf("%d ", array[i][j]);
+    }
+    printf("\n");
+  }
+}
+
+#include <iostream>
+
+// X x Y x Z matrix
+#define X 2
+#define Y 3
+#define Z 4
+
+// Dynamic Memory Allocation in C++ for 3D Array
+int main()
+{
+	// dynamically allocate memory of size X*Y*Z
+	int* A = new int[X * Y * Z];
+
+	// assign values to allocated memory
+	for (int i = 0; i < X; i++)
+		for (int j = 0; j < Y; j++)
+			for (int k = 0; k < Z; k++)
+				*(A + i*Y*Z + j*Z + k) = rand() % 100;
+
+	// print the 3D array
+	for (int i = 0; i < X; i++)
+	{
+		for (int j = 0; j < Y; j++)
+		{
+			for (int k = 0; k < Z; k++)
+				std::cout << *(A + i*Y*Z + j*Z + k) << " ";
+
+			std::cout << std::endl;
+		}
+		std::cout << std::endl;
+	}
+
+	// deallocate memory
+	delete[] A;
+
+	return 0;
+}
+
+*/
--- LCA_1BST/LCA_BST.cpp
+++ LCA_1BST/LCA_BST.cpp
@@ -89,3 +89,169 @@ node *lca(node *root, int v1, int v2)
 
 
 }
+
+
+/* C++ Program to find LCA of n1 and n2 using one traversal of Binary Tree */
+#include <iostream> 
+
+using namespace std;
+
+// A Binary Tree Node 
+struct Node
+{
+    struct Node* left, * right;
+    int key;
+};
+
+
+
+
+//Example code copied from: https://www.geeksforgeeks.org/lowest-common-ancestor-binary-tree-set-1/
+
+/*Binary Search Tree
+/* Function to find LCA of n1 and n2. The function assumes that both
+   n1 and n2 are present in BST */
+/*
+struct node* lca(struct node* root, int n1, int n2)
+{
+    if (root == NULL) return NULL;
+
+    // If both n1 and n2 are smaller than root, then LCA lies in left 
+    if (root->data > n1 && root->data > n2)
+        return lca(root->left, n1, n2);
+
+    // If both n1 and n2 are greater than root, then LCA lies in right 
+    if (root->data < n1 && root->data < n2)
+        return lca(root->right, n1, n2);
+
+    return root;
+} 
+*/
+
+	//Good explanation
+// Utility function to create a new tree Node 
+Node* newNode(int key)
+{
+    Node* temp = new Node;
+    temp->key = key;
+    temp->left = temp->right = NULL;
+    return temp;
+}
+
+// This function returns pointer to LCA of two given values n1 and n2. 
+// This function assumes that n1 and n2 are present in Binary Tree 
+struct Node* findLCA(struct Node* root, int n1, int n2)
+{
+    // Base case 
+    if (root == NULL) return NULL;
+
+    // If either n1 or n2 matches with root's key, report 
+    // the presence by returning root (Note that if a key is 
+    // ancestor of other, then the ancestor key becomes LCA 
+    if (root->key == n1 || root->key == n2)
+        return root;
+
+    // Look for keys in left and right subtrees 
+    Node* left_lca = findLCA(root->left, n1, n2);
+    Node* right_lca = findLCA(root->right, n1, n2);
+
+    // If both of the above calls return Non-NULL, then one key 
+    // is present in once subtree and other is present in other, 
+    // So this node is the LCA 
+    if (left_lca && right_lca)  return root;
+
+    // Otherwise check if left subtree or right subtree is LCA 
+    return (left_lca != NULL) ? left_lca : right_lca;
+}
+
+
+/*
+// Driver program to test above functions 
+int main()
+{
+    // Let us create binary tree given in the above example 
+    Node* root = newNode(1);
+    root->left = newNode(2);
+    root->right = newNode(3);
+    root->left->left = newNode(4);
+    root->left->right = newNode(5);
+    root->right->left = newNode(6);
+    root->right->right = newNode(7);
+    cout << "LCA(4, 5) = " << findLCA(root, 4, 5)->key;
+    cout << "nLCA(4, 6) = " << findLCA(root, 4, 6)->key;
+    cout << "nLCA(3, 4) = " << findLCA(root, 3, 4)->key;
+    cout << "nLCA(2, 4) = " << findLCA(root, 2, 4)->key;
+    return 0;
+}
+*/
+
+
+//Other Approach:, Two vectors
+/// C++ Program for Lowest Common Ancestor in a Binary Tree
+//// A O(n) solution to find LCA of two given values n1 and n2 
+//#include <iostream> 
+//#include <vector> 
+//
+//using namespace std;
+//
+//// A Binary Tree node 
+//struct Node
+//{
+//    int key;
+//    struct Node* left, * right;
+//};
+//
+//// Utility function creates a new binary tree node with given key 
+//Node* newNode(int k)
+//{
+//    Node* temp = new Node;
+//    temp->key = k;
+//    temp->left = temp->right = NULL;
+//    return temp;
+//}
+//
+//// Finds the path from root node to given root of the tree, Stores the 
+//// path in a vector path[], returns true if path exists otherwise false 
+//bool findPath(Node * root, vector<int> & path, int k)
+//{
+//    // base case 
+//    if (root == NULL) return false;
+//
+//    // Store this node in path vector. The node will be removed if 
+//    // not in path from root to k 
+//    path.push_back(root->key);
+//
+//    // See if the k is same as root's key 
+//    if (root->key == k)
+//        return true;
+//
+//    // Check if k is found in left or right sub-tree 
+//    if ((root->left && findPath(root->left, path, k)) ||
+//        (root->right && findPath(root->right, path, k)))
+//        return true;
+//
+//    // If not present in subtree rooted with root, remove root from 
+//    // path[] and return false 
+//    path.pop_back();
+//    return false;
+//}
+//
+//// Returns LCA if node n1, n2 are present in the given binary tree, 
+//// otherwise return -1 
+//int findLCA(Node * root, int n1, int n2)
+//{
+//    // to store paths to n1 and n2 from the root 
+//    vector<int> path1, path2;
+//
+//    // Find paths from root to n1 and root to n1. If either n1 or n2 
+//    // is not present, return -1 
+//    if (!findPath(root, path1, n1) || !findPath(root, path2, n2))
+//        return -1;
+//
+//    /* Compare the paths to get the first different value */
+//    int i;
+//    for (i = 0; i < path1.size() && i < path2.size(); i++)
+//        if (path1[i] != path2[i])
+//            break;
+//    return path1[i - 1];
+//}
